7410f6c560b9ffc01f869f48a78841459a839936
Matt Latham <latham75@gmail.com>
2016-06-17 12:56:23 -0500

    finished functions, fixing issues

--- Predator.js
+++ Predator.js
@@ -5,14 +5,23 @@ var Predator = (function () {
   return {
     loadCarnivores: function (callbackToInvoke) {
       var loader = new XMLHttpRequest();
+      loader.open("GET", "carnivores.json");
+      loader.send();
 
       loader.addEventListener("load", function () {
-        // Set the value of the private array
-        carnivores = JSON.parse(this.responseText);
+        carns = JSON.parse(this.responseText).carnivores;
+        callbackToInvoke(carns);
 
-        // Invoke the callback function so that the caller knows
-        // that the process is complete. Make sure to pass the 
-        // carnivore array as an argument.
+  return {
+    loadHerbivores: function (callbackToInvoke) {
+      var loader = new XMLHttpRequest();
+      loader.open("GET", "herbivores.json");
+      loader.send();
+
+      loader.addEventListener("load", function () {
+        herbs = JSON.parse(this.responseText).herbivores;
+        callbackToInvoke(herbs);
+
 
       });
     }
--- iife-xhr.js
+++ iife-xhr.js
@@ -1,9 +1,35 @@
-function showCarnivores (carnivores) {
-	// to load into DOM
+var Predator = (function showCarnivores (carnivores) {
+	var listStart = document.createElement('ul');
+	listStart.className = "title";
+	document.getElementById("carnies").appendChild(listStart);
+	listStart.appendChild(document.createTextNode("Carnivores"));
+	function domInate (element, index, array){
+		var dino = document.createElement("li");
+		listStart.appendChild(dino);
+		dino.className = "property";
+		dino.appendChild(document.createTextNode(element));
+	}
+	carnivores.forEach(domInate);
 }
 
-function showHerbivores () {
-	// to load into DOM
+function showHerbivores (herbivores) {
+	var listStart = document.createElement('ul');
+	listStart.className = "title";
+	document.getElementById("herbies").appendChild(listStart);
+	listStart.appendChild(document.createTextNode("Herbivores"));
+	function domInate (element, index, array){
+		var dino = document.createElement("li");
+		listStart.appendChild(dino);
+		dino.className = "property";
+		dino.appendChild(document.createTextNode(element));
+	}
+	herbivores.forEach(domInate);
 }
+
 
 Predator.loadCarnivores(showCarnivores);
+
+Predator.loadHerbivores(showHerbivores);
+
+
+);
